c85ca23880f9f987e61c049a87bf8d15470700ed
imafurrylol <admin@imafurry.lol>
2026-09-20 21:45:18 +0000

    Create GameSettings.lua -- pretty useless, but useful when you need UserSettings().GameSettings stuff

---
+++ GameSettings.lua
@@ -0,0 +1,60 @@
+local gameSettings = UserSettings().GameSettings
+local GameSettings = {}
+local listeners = {}
+
+gameSettings.Changed:Connect(function(setting)
+	local success, value = pcall(function()
+		return gameSettings[setting]
+	end)
+
+	if not success then return end
+	local settingListeners = listeners[setting]
+	if settingListeners == nil then return end	
+
+	for _, listener in ipairs(settingListeners) do
+		local _success, _error = pcall(listener.Callback, value)
+
+		if not _success then
+			warn("listener for " .. setting ..  " errored: " .. _error)
+		end
+	end
+end)
+
+function GameSettings.Get(name)
+	if name == nil then
+		return gameSettings
+	end
+
+	local success, value = pcall(function()
+		return gameSettings[name]
+	end)
+
+	if not success then return nil end
+
+	return value
+end	
+
+function GameSettings.OnChange(settingName, callback)
+	local self = {
+		Name = settingName,
+		Callback = callback,
+		Disconnect = function()
+			for i, listener in ipairs(listeners[settingName]) do
+				if listener == self then
+					table.remove(listeners[settingName], i)
+					break
+				end
+			end
+
+			if #listeners[settingName] == 0 then
+				listeners[settingName] = nil
+			end
+		end
+	}
+
+	listeners[settingName] = listeners[settingName] or {}
+	table.insert(listeners[settingName], self)
+	return self
+end
+
+return GameSettings
