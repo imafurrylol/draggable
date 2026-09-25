2cd7b15d0aa8bcc6a1f6ecd2d49fa6083aa69664
imafurrylol <admin@imafurry.lol>
2026-09-25 18:24:01 +0000

    Delete Animation directory

--- Animation/Lerp.lua
+++
@@ -1,9 +0,0 @@
--- created by @xvZiuV9zoZsEqkfWwyWc on Roblox
-local Animation = require(script.Parent)
-local LerpAnimation = Animation:Extend()
-
-function LerpAnimation:Evaluate(Start, Target, Progress)
-	return Start + (Target - Start) * Progress
-end
-
-return LerpAnimation
